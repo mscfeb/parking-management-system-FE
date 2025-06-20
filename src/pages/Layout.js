8f6f3296c8b5303fc1b8a43a13e6ddc4f1f43d71
mscfeb <mohammedsaadchaush@gmail.com>
2025-06-21 04:43:07 +0530

    change

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -25,7 +25,7 @@ const Layout = () => {
         <div className="main-container">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
-                    <a className="navbar-brand" href="#">Rent a spot</a>
+                    <a className="navbar-brand" href="#">Parking Management System</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -89,7 +89,6 @@ const Layout = () => {
             <footer className="container-fluid p-5 mt-5">
                 <div className="row">
                     <div className="col-md-4">
-                        <p>Copyright &copy; 2023 by Harshadkumar Patel</p>
                     </div>
                     <div className="col-md-4">
                     </div>
